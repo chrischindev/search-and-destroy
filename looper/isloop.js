ef28e36066b1071ca07331ef3c146657e314d9b7
Chris Chin <chin.christopher@gmail.com>
2019-10-23 16:21:05 -0400

    loopy linked list

--- looper/isloop.js
+++ looper/isloop.js
@@ -1,10 +1,23 @@
-'use strict';
+"use strict";
 
 //Complete this algo
-const isLoop = (linkedlist) => {
-
+const isLoop = linkedlist => {
+  let hashTable = {};
+  let currentNode = linkedlist.head;
+  // we loop until we reach the tail, or until we reach a duplicate value
+  while (currentNode) {
+    let value = currentNode.value;
+    // if we encounter a new value, we store it in the hashTable
+    if (!hashTable[value]) {
+      hashTable[value] = true;
+      // moving to the next node
+      currentNode = currentNode.next;
+    }
+    // else we've seen this value before, so we have encountered a loop
+    else return true;
+  }
+  return false;
 };
-
 
 /*
 EXTRA CREDIT:
@@ -14,4 +27,4 @@ This function should return the Node value the loop begins at
 Remember to write some test specs too!
 
 */
-module.exports = isLoop
+module.exports = isLoop;
