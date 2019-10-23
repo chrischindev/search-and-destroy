3cbd6e402a6e255239a63608432c9fbe447f5008
Alex Paul <al.9thousand@gmail.com>
2019-10-23 15:32:43 -0400

    finished binary search

--- binary-search/binary-search.js
+++ binary-search/binary-search.js
@@ -2,8 +2,23 @@
 
 // Complete this algo
 const binarySearch = (array, target) => {
-	
+	debugger
+	const midPoint = Math.floor(array.length / 2)
+	const leftArr = array.slice(0, midPoint)
+	const rightArr = array.slice(midPoint)
+
+	if (target === rightArr[0] || target === leftArr[leftArr.length - 1]) return true
+	if (target > rightArr[0]) {
+		if (rightArr.length === 1) return false
+		return binarySearch(rightArr, target)
+	} 
+	else if (target < leftArr[leftArr.length - 1]) {
+		if (leftArr.length === 1) return false
+		return binarySearch(leftArr, target)
+	} return false
 };
+
+console.log(binarySearch([-0.5, 0.03, 2.6, 3, 3.5, 3.777777, 5], 0.02))
 
 /*
 	EXTRA CREDIT:
